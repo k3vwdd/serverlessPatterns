83963f8b9eea90d31e4294f1d7e75338c47b6f0c
k3vwdd <kevwdiaz@outlook.com>
2025-01-26 20:04:38 -0600

    need to fix update to db..

--- assets/src/UsersFunction/users.ts
+++ assets/src/UsersFunction/users.ts
@@ -1,30 +1,60 @@
-import { Handler } from "aws-lambda";
-import { configDotenv } from "dotenv";
+import { APIGatewayProxyEvent } from "aws-lambda";
+import { DeleteCommand, DynamoDBDocument, GetCommand, PutCommand, ScanCommand } from "@aws-sdk/lib-dynamodb";
+import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
+const client = new DynamoDBClient({});
+const ddbDocClient = DynamoDBDocument.from(client);
 
+const USERS_TABLE = process.env.USERS_TABLE || "";
 
-
-const USERS_TABLE = process.env.USERS_TABLE;
-
-
-
-export async function handler(event: Handler) {
+export async function handler(event: APIGatewayProxyEvent) {
     console.log(`Event recieved: ${JSON.stringify(event, null, 2)}`);
-    console.log(USERS_TABLE);
 
-    const ip = "http://checkip.amazonaws.com/";
+    const routeKey = `${event.httpMethod} ${event.resource}`;
+
     try {
-        const response = await fetch(ip);
-        if (!response.ok) {
-            throw new Error(`response status: ${response.status}`);
-        };
-        return {
-            statusCode: 200,
-            body: JSON.stringify({
-                message: "Yeeer",
-                location: `Your ip is ${response.text()}`
-            }),
+        switch (routeKey) {
+            default:
+                return {
+                    Message: "Unsupported route",
+                    statusCode: 400,
+                    headers: {
+                        "Content-Type": "application/json",
+                        "Access-Control-Allow-Origin": "*",
+                    }
+                };
+            case "GET /users":
+                const scanResponse = new ScanCommand({TableName: USERS_TABLE, Select: "ALL_ATTRIBUTES"});
+                const sData = await ddbDocClient.send(scanResponse);
+                return {
+                    Message: sData.Items,
+                    statusCode: 200,
+                };
+            case "GET /users/{userid}":
+                const getResponse = new GetCommand({TableName: USERS_TABLE, Key: {userid: `${event.pathParameters?.userid}`}});
+                const gData = await ddbDocClient.send(getResponse);
+                return {
+                    Message: gData.Item ?? "",
+                    ...(gData.Item ? { statusCode: 200 } : {})
+                };
+            case "DELETE /users/{userid}":
+                const delResponse = new DeleteCommand({TableName: USERS_TABLE, Key: {userid: `${event.pathParameters?.userid}`}});
+                const dData = await ddbDocClient.send(delResponse);
+                return {
+                    Message: {},
+                    statusCode: 200,
+                };
+            case "POST /users":
+                const requestJson = JSON.parse(event?.body ?? "{}");
+                requestJson.timestamp = new Date().toISOString();
+                !("userid" in requestJson) ? requestJson.userid = crypto.randomUUID() : requestJson.userid;
+                const updateResponse = new PutCommand({TableName: USERS_TABLE, Item: requestJson });
+                const uData = await ddbDocClient.send(updateResponse);
+                return {
+                    Message: requestJson,
+                    statusCode: 200,
+                };
         }
     } catch (error) {
-        throw error;
+        throw error
     }
-};
+}
