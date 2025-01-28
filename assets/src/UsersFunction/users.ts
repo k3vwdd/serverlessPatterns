fc273093f4965b8373ce8c328dc0565210823638
k3vwdd <kevwdiaz@outlook.com>
2025-01-28 13:52:12 -0600

    got logic to check if user is already in db table : )

--- assets/src/UsersFunction/users.ts
+++ assets/src/UsersFunction/users.ts
@@ -1,59 +1,81 @@
-import { APIGatewayProxyEvent } from "aws-lambda";
-import { DeleteCommand, DynamoDBDocument, GetCommand, PutCommand, ScanCommand } from "@aws-sdk/lib-dynamodb";
+import { Context, APIGatewayProxyEvent, APIGatewayProxyResult } from "aws-lambda";
+import { DeleteCommand, DynamoDBDocument, GetCommand, PutCommand, ScanCommand, QueryCommand } from "@aws-sdk/lib-dynamodb";
+import { marshall } from "@aws-sdk/util-dynamodb";
 import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
 const client = new DynamoDBClient({});
 const ddbDocClient = DynamoDBDocument.from(client);
 
 const USERS_TABLE = process.env.USERS_TABLE || "";
+//const USERS_TABLE = "testUsers";
 
-export async function handler(event: APIGatewayProxyEvent) {
+enum UserRoutes  {
+    CREATE_USER = "POST /users",
+        DELETE_USER = "DELETE /users/{userid}",
+        GET_USER = "GET /users/{userid}",
+        GET_USERS = "GET /users",
+        UPDATE_USER = "PUT /users/{userid}",
+}
+const defaultHeaders = {
+    "Content-Type": "application/json",
+    "Access-Control-Allow-Origin": "*",
+}
+
+
+async function createUser(event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> {
+    try {
+        const body = JSON.parse(event?.body || "{}");
+        console.log(body);
+        let userName = body.username;
+        const response = await ddbDocClient.send(new QueryCommand({
+            TableName: USERS_TABLE,
+            IndexName: "username",
+            KeyConditionExpression: "username = :username",
+            ExpressionAttributeValues: {":username": userName},
+        }))
+        if (response.Items?.length! > 0) {
+            return {
+                statusCode: 200,
+                headers: { ...defaultHeaders },
+                body: "User already in db"
+            }
+        } else {
+            body['timestamp'] = new Date().toISOString();
+            body['userid'] = crypto.randomUUID();
+            await ddbDocClient.send(new PutCommand({
+                TableName: USERS_TABLE,
+                Item: {
+                    ...body,
+                },
+                ConditionExpression: "attribute_not_exists(username)",
+            }));
+            return {
+                statusCode: 201,
+                headers: { ...defaultHeaders },
+                body: JSON.stringify(body),
+            }
+        }
+    } catch (error) {
+        throw error;
+    }
+};
+
+export async function handler(event: APIGatewayProxyEvent, context: Context): Promise<APIGatewayProxyResult> {
     console.log(`Event recieved: ${JSON.stringify(event, null, 2)}`);
-
     const routeKey = `${event.httpMethod} ${event.resource}`;
 
     try {
         switch (routeKey) {
+            case UserRoutes.CREATE_USER:
+                 return await createUser(event);
             default:
                 return {
-                    Message: "Unsupported route",
                     statusCode: 400,
-                    headers: {
-                        "Content-Type": "application/json",
-                        "Access-Control-Allow-Origin": "*",
-                    }
-                };
-            case "GET /users":
-                const scanResponse = new ScanCommand({TableName: USERS_TABLE, Select: "ALL_ATTRIBUTES"});
-                const sData = await ddbDocClient.send(scanResponse);
-                return {
-                    Message: sData.Items,
-                    statusCode: 200,
-                };
-            case "GET /users/{userid}":
-                const getResponse = new GetCommand({TableName: USERS_TABLE, Key: {userid: `${event.pathParameters?.userid}`}});
-                const gData = await ddbDocClient.send(getResponse);
-                return {
-                    Message: gData.Item ?? "",
-                    ...(gData.Item ? { statusCode: 200 } : {})
-                };
-            case "DELETE /users/{userid}":
-                const delResponse = new DeleteCommand({TableName: USERS_TABLE, Key: {userid: `${event.pathParameters?.userid}`}});
-                const dData = await ddbDocClient.send(delResponse);
-                return {
-                    Message: {},
-                    statusCode: 200,
-                };
-            case "POST /users":
-                const requestJson = JSON.parse(event?.body ?? "{}");
-                requestJson.timestamp = new Date().toISOString();
-                !("userid" in requestJson) ? requestJson.userid = crypto.randomUUID() : requestJson.userid;
-                const updateResponse = new PutCommand({TableName: USERS_TABLE, Item: requestJson });
-                const uData = await ddbDocClient.send(updateResponse);
-                return {
-                    Message: requestJson,
-                    statusCode: 200,
-                };
-        }
+                    headers: { ...defaultHeaders },
+                    body: JSON.stringify({
+                        message: "Unsupported route",
+                    }),
+                }
+            }
     } catch (error) {
         throw error
     }
